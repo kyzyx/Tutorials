e761fbdac9e3964e700d72860044eb34014695ae
Edward Zhang <edward64@gmail.com>
2015-10-30 17:33:22 -0700

    Update Kinectv2 Tutorial 4

--- Kinect2SDK/4_SkeletalTracking/main.cpp
+++ Kinect2SDK/4_SkeletalTracking/main.cpp
@@ -7,149 +7,148 @@
 #include <Windows.h>
 #include <Ole2.h>
 
-#include <NuiApi.h>
-#include <NuiImageCamera.h>
-#include <NuiSensor.h>
+#include <Kinect.h>
 
-// OpenGL Variables
-long depthToRgbMap[width*height*2];
 // We'll be using buffer objects to store the kinect point cloud
 GLuint vboId;
 GLuint cboId;
 
-// Kinect variables
-HANDLE depthStream;
-HANDLE rgbStream;
-INuiSensor* sensor;
+// Intermediate Buffers
+unsigned char rgbimage[colorwidth*colorheight*4];    // Stores RGB color image
+ColorSpacePoint depth2rgb[width*height];             // Maps depth pixels to rgb pixels
+CameraSpacePoint depth2xyz[width*height];			 // Maps depth pixels to 3d coordinates
 
-// Stores the coordinates of each joint
-Vector4 skeletonPosition[NUI_SKELETON_POSITION_COUNT];
+// Body tracking variables
+BOOLEAN tracked;							// Whether we see a body
+Joint joints[JointType_Count];				// List of joints in the tracked body
+
+// Kinect Variables
+IKinectSensor* sensor;             // Kinect sensor
+IMultiSourceFrameReader* reader;   // Kinect data source
+ICoordinateMapper* mapper;         // Converts between depth, color, and 3d coordinates
 
 bool initKinect() {
-    // Get a working kinect sensor
-    int numSensors;
-    if (NuiGetSensorCount(&numSensors) < 0 || numSensors < 1) return false;
-    if (NuiCreateSensorByIndex(0, &sensor) < 0) return false;
+    if (FAILED(GetDefaultKinectSensor(&sensor))) {
+		return false;
+	}
+	if (sensor) {
+		sensor->get_CoordinateMapper(&mapper);
 
-    // Initialize sensor
-    sensor->NuiInitialize(NUI_INITIALIZE_FLAG_USES_DEPTH_AND_PLAYER_INDEX | NUI_INITIALIZE_FLAG_USES_COLOR | NUI_INITIALIZE_FLAG_USES_SKELETON);
-    sensor->NuiImageStreamOpen(NUI_IMAGE_TYPE_DEPTH_AND_PLAYER_INDEX, // Depth camera or rgb camera?
-        NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
-        0,        // Image stream flags, e.g. near mode
-        2,        // Number of frames to buffer
-        NULL,     // Event handle
-        &depthStream);
-	sensor->NuiImageStreamOpen(NUI_IMAGE_TYPE_COLOR, // Depth camera or rgb camera?
-        NUI_IMAGE_RESOLUTION_640x480,                // Image resolution
-        0,      // Image stream flags, e.g. near mode
-        2,      // Number of frames to buffer
-        NULL,   // Event handle
-		&rgbStream);
-	sensor->NuiSkeletonTrackingEnable(NULL, 0); // NUI_SKELETON_TRACKING_FLAG_ENABLE_SEATED_SUPPORT for only upper body
-    return sensor;
-}
-
-void getDepthData(GLubyte* dest) {
-	float* fdest = (float*) dest;
-	long* depth2rgb = (long*) depthToRgbMap;
-    NUI_IMAGE_FRAME imageFrame;
-    NUI_LOCKED_RECT LockedRect;
-    if (sensor->NuiImageStreamGetNextFrame(depthStream, 0, &imageFrame) < 0) return;
-    INuiFrameTexture* texture = imageFrame.pFrameTexture;
-    texture->LockRect(0, &LockedRect, NULL, 0);
-    if (LockedRect.Pitch != 0) {
-        const USHORT* curr = (const USHORT*) LockedRect.pBits;
-        for (int j = 0; j < height; ++j) {
-			for (int i = 0; i < width; ++i) {
-				// Get depth of pixel in millimeters
-				USHORT depth = NuiDepthPixelToDepth(*curr++);
-				// Store coordinates of the point corresponding to this pixel
-				Vector4 pos = NuiTransformDepthImageToSkeleton(i, j, depth<<3, NUI_IMAGE_RESOLUTION_640x480);
-				*fdest++ = pos.x/pos.w;
-				*fdest++ = pos.y/pos.w;
-				*fdest++ = pos.z/pos.w;
-				// Store the index into the color array corresponding to this pixel
-				NuiImageGetColorPixelCoordinatesFromDepthPixelAtResolution(
-					NUI_IMAGE_RESOLUTION_640x480, NUI_IMAGE_RESOLUTION_640x480, NULL,
-					i, j, depth<<3, depth2rgb, depth2rgb+1);
-				depth2rgb += 2;
-			}
-		}
-    }
-    texture->UnlockRect(0);
-    sensor->NuiImageStreamReleaseFrame(depthStream, &imageFrame);
-}
-
-void getRgbData(GLubyte* dest) {
-	float* fdest = (float*) dest;
-	long* depth2rgb = (long*) depthToRgbMap;
-	NUI_IMAGE_FRAME imageFrame;
-    NUI_LOCKED_RECT LockedRect;
-    if (sensor->NuiImageStreamGetNextFrame(rgbStream, 0, &imageFrame) < 0) return;
-    INuiFrameTexture* texture = imageFrame.pFrameTexture;
-    texture->LockRect(0, &LockedRect, NULL, 0);
-    if (LockedRect.Pitch != 0) {
-        const BYTE* start = (const BYTE*) LockedRect.pBits;
-        for (int j = 0; j < height; ++j) {
-			for (int i = 0; i < width; ++i) {
-				// Determine rgb color for each depth pixel
-				long x = *depth2rgb++;
-				long y = *depth2rgb++;
-				// If out of bounds, then don't color it at all
-				if (x < 0 || y < 0 || x > width || y > height) {
-					for (int n = 0; n < 3; ++n) *(fdest++) = 0.0f;
-				}
-				else {
-					const BYTE* curr = start + (x + width*y)*4;
-					for (int n = 0; n < 3; ++n) *(fdest++) = curr[2-n]/255.0f;
-				}
-
-			}
-		}
-    }
-    texture->UnlockRect(0);
-    sensor->NuiImageStreamReleaseFrame(rgbStream, &imageFrame);
-}
-
-void getSkeletalData() {
-	NUI_SKELETON_FRAME skeletonFrame = {0};
-    if (sensor->NuiSkeletonGetNextFrame(0, &skeletonFrame) >= 0) {
-		sensor->NuiTransformSmooth(&skeletonFrame, NULL);
-		// Loop over all sensed skeletons
-		for (int z = 0; z < NUI_SKELETON_COUNT; ++z) {
-			const NUI_SKELETON_DATA& skeleton = skeletonFrame.SkeletonData[z];
-			// Check the state of the skeleton
-			if (skeleton.eTrackingState == NUI_SKELETON_TRACKED) {			
-				// Copy the joint positions into our array
-				for (int i = 0; i < NUI_SKELETON_POSITION_COUNT; ++i) {
-					skeletonPosition[i] = skeleton.SkeletonPositions[i];
-					if (skeleton.eSkeletonPositionTrackingState[i] == NUI_SKELETON_POSITION_NOT_TRACKED) {
-						skeletonPosition[i].w = 0;
-					}
-				}
-				return; // Only take the data for one skeleton
-			}
-		}
+		sensor->Open();
+		sensor->OpenMultiSourceFrameReader(
+			FrameSourceTypes::FrameSourceTypes_Depth | FrameSourceTypes::FrameSourceTypes_Color | FrameSourceTypes::FrameSourceTypes_Body,
+			&reader);
+		return reader;
+	} else {
+		return false;
 	}
 }
+
+void getDepthData(IMultiSourceFrame* frame, GLubyte* dest) {
+	IDepthFrame* depthframe;
+	IDepthFrameReference* frameref = NULL;
+	frame->get_DepthFrameReference(&frameref);
+	frameref->AcquireFrame(&depthframe);
+	if (frameref) frameref->Release();
+
+	if (!depthframe) return;
+
+	// Get data from frame
+	unsigned int sz;
+	unsigned short* buf;
+	depthframe->AccessUnderlyingBuffer(&sz, &buf);
+
+	// Write vertex coordinates
+	mapper->MapDepthFrameToCameraSpace(sz, buf, width*height, depth2xyz);
+	float* fdest = (float*)dest;
+	for (int i = 0; i < sz; i++) {
+		*fdest++ = depth2xyz[i].X;
+		*fdest++ = depth2xyz[i].Y;
+		*fdest++ = depth2xyz[i].Z;
+	}
+
+	// Fill in depth2rgb map
+	mapper->MapDepthFrameToColorSpace(sz, buf, width*height, depth2rgb);
+	if (depthframe) depthframe->Release();
+}
+
+void getRgbData(IMultiSourceFrame* frame, GLubyte* dest) {
+	IColorFrame* colorframe;
+	IColorFrameReference* frameref = NULL;
+	frame->get_ColorFrameReference(&frameref);
+	frameref->AcquireFrame(&colorframe);
+	if (frameref) frameref->Release();
+
+	if (!colorframe) return;
+
+	// Get data from frame
+	colorframe->CopyConvertedFrameDataToArray(colorwidth*colorheight*4, rgbimage, ColorImageFormat_Rgba);
+
+	// Write color array for vertices
+	ColorSpacePoint* p = depth2rgb;
+	float* fdest = (float*)dest;
+	for (int i = 0; i < height; i++) {
+		for (int j = 0; j < width; j++) {
+			if (p->X < 0 || p->Y < 0 || p->X > colorwidth || p->Y > colorheight) {
+				*fdest++ = 0;
+				*fdest++ = 0;
+				*fdest++ = 0;
+			}
+			else {
+				int idx = (int)p->X + colorwidth*(int)p->Y;
+				*fdest++ = rgbimage[4 * idx + 0]/255.;
+				*fdest++ = rgbimage[4 * idx + 1]/255.;
+				*fdest++ = rgbimage[4 * idx + 2]/255.;
+			}
+			// Don't copy alpha channel
+			p++;
+		}
+	}
+
+	if (colorframe) colorframe->Release();
+}
+
+void getBodyData(IMultiSourceFrame* frame) {
+	IBodyFrame* bodyframe;
+	IBodyFrameReference* frameref = NULL;
+	frame->get_BodyFrameReference(&frameref);
+	frameref->AcquireFrame(&bodyframe);
+	if (frameref) frameref->Release();
+
+	if (!bodyframe) return;
+
+	IBody* body[BODY_COUNT] = { 0 };
+	bodyframe->GetAndRefreshBodyData(BODY_COUNT, body);
+	for (int i = 0; i < BODY_COUNT; i++) {
+		body[i]->get_IsTracked(&tracked);
+		if (tracked) {
+			body[i]->GetJoints(JointType_Count, joints);
+			break;
+		}
+	}
+
+	if (bodyframe) bodyframe->Release();
+}
+
 void getKinectData() {
-	const int dataSize = width*height*3*4;
-	GLubyte* ptr;
-	glBindBuffer(GL_ARRAY_BUFFER, vboId);
-	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
-	ptr = (GLubyte*) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-	if (ptr) {
-		getDepthData(ptr);
+	IMultiSourceFrame* frame = NULL;
+	if (SUCCEEDED(reader->AcquireLatestFrame(&frame))) {
+		GLubyte* ptr;
+		glBindBuffer(GL_ARRAY_BUFFER, vboId);
+		ptr = (GLubyte*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+		if (ptr) {
+			getDepthData(frame, ptr);
+		}
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+		glBindBuffer(GL_ARRAY_BUFFER, cboId);
+		ptr = (GLubyte*)glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
+		if (ptr) {
+			getRgbData(frame, ptr);
+		}
+		glUnmapBuffer(GL_ARRAY_BUFFER);
+        getBodyData(frame);
 	}
-	glUnmapBuffer(GL_ARRAY_BUFFER);
-	glBindBuffer(GL_ARRAY_BUFFER, cboId);
-	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
-	ptr = (GLubyte*) glMapBuffer(GL_ARRAY_BUFFER, GL_WRITE_ONLY);
-	if (ptr) {
-		getRgbData(ptr);
-	}
-	glUnmapBuffer(GL_ARRAY_BUFFER);
-	getSkeletalData();
+	if (frame) frame->Release();
 }
 
 void rotateCamera() {
@@ -160,7 +159,7 @@ void rotateCamera() {
 	glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
 	gluLookAt(x,0,z,0,0,radius/2,0,1,0);
-	angle += 0.05;
+	angle += 0.001;
 }
 
 void drawKinectData() {
@@ -173,38 +172,36 @@ void drawKinectData() {
 
 	glBindBuffer(GL_ARRAY_BUFFER, vboId);
 	glVertexPointer(3, GL_FLOAT, 0, NULL);
-	
+
 	glBindBuffer(GL_ARRAY_BUFFER, cboId);
 	glColorPointer(3, GL_FLOAT, 0, NULL);
 
 	glPointSize(1.f);
 	glDrawArrays(GL_POINTS, 0, width*height);
-	
+
 	glDisableClientState(GL_VERTEX_ARRAY);
 	glDisableClientState(GL_COLOR_ARRAY);
-	
-	// Draw some arms
-	const Vector4& lh = skeletonPosition[NUI_SKELETON_POSITION_HAND_LEFT];
-	const Vector4& le = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_LEFT];
-	const Vector4& ls = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_LEFT];
-	const Vector4& rh = skeletonPosition[NUI_SKELETON_POSITION_HAND_RIGHT];
-	const Vector4& re = skeletonPosition[NUI_SKELETON_POSITION_ELBOW_RIGHT];
-	const Vector4& rs = skeletonPosition[NUI_SKELETON_POSITION_SHOULDER_RIGHT];
-	glBegin(GL_LINES);
+
+	if (tracked) {
+		// Draw some arms
+		const CameraSpacePoint& lh = joints[JointType_WristLeft].Position;
+		const CameraSpacePoint& le = joints[JointType_ElbowLeft].Position;;
+		const CameraSpacePoint& ls = joints[JointType_ShoulderLeft].Position;;
+		const CameraSpacePoint& rh = joints[JointType_WristRight].Position;;
+		const CameraSpacePoint& re = joints[JointType_ElbowRight].Position;;
+		const CameraSpacePoint& rs = joints[JointType_ShoulderRight].Position;;
+		glBegin(GL_LINES);
 		glColor3f(1.f, 0.f, 0.f);
-		if (lh.w > 0 && le.w > 0 && ls.w > 0) {
-			glVertex3f(lh.x, lh.y, lh.z);
-			glVertex3f(le.x, le.y, le.z);
-			glVertex3f(le.x, le.y, le.z);
-			glVertex3f(ls.x, ls.y, ls.z);
-		}
-		if (rh.w > 0 && re.w > 0 && rs.w > 0) {
-			glVertex3f(rh.x, rh.y, rh.z);
-			glVertex3f(re.x, re.y, re.z);
-			glVertex3f(re.x, re.y, re.z);
-			glVertex3f(rs.x, rs.y, rs.z);
-		}
-	glEnd();
+		glVertex3f(lh.X, lh.Y, lh.Z);
+		glVertex3f(le.X, le.Y, le.Z);
+		glVertex3f(le.X, le.Y, le.Z);
+		glVertex3f(ls.X, ls.Y, ls.Z);
+		glVertex3f(rh.X, rh.Y, rh.Z);
+		glVertex3f(re.X, re.Y, re.Z);
+		glVertex3f(re.X, re.Y, re.Z);
+		glVertex3f(rs.X, rs.Y, rs.Z);
+		glEnd();
+	}
 }
 
 int main(int argc, char* argv[]) {
@@ -216,10 +213,13 @@ int main(int argc, char* argv[]) {
     glClearDepth(1.0f);
 
 	// Set up array buffers
+	const int dataSize = width*height * 3 * 4;
 	glGenBuffers(1, &vboId);
 	glBindBuffer(GL_ARRAY_BUFFER, vboId);
+	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
 	glGenBuffers(1, &cboId);
 	glBindBuffer(GL_ARRAY_BUFFER, cboId);
+	glBufferData(GL_ARRAY_BUFFER, dataSize, 0, GL_DYNAMIC_DRAW);
 
     // Camera setup
     glViewport(0, 0, width, height);
